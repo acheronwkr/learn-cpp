3ab82366877abf2371b41e0f6949a522839440f7
Borys <jozwiak.borys@gmail.com>
2026-03-09 13:01:20 +0100

    Replace add_hobby with new_hobby method
    Also added using namespace std for better readability.

--- 7-classes-and-objects/app.cpp
+++ 7-classes-and-objects/app.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "profile.hpp"
 
+using namespace std;
+
 int main() {
 
-  Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
-  sam.add_hobby("listening to audiobooks and podcasts");
-  sam.add_hobby("playing rec sports like bowling and kickball");
-  sam.add_hobby("writing a speculative fiction novel");
-  sam.add_hobby("reading advice columns");
-  std::cout << sam.view_profile();
-
+    Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
+    sam.new_hobby("listening to audiobooks and podcasts");
+    sam.new_hobby("playing rec sports like bowling and kickball");
+    sam.new_hobby("writing a speculative fiction novel");
+    sam.new_hobby("reading advice columns");
+    cout << sam.view_profile();
 }
